e5fbeb54478faea8b38a09df4245c842f8dba34e
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-06-23 10:00:48 +0200

    Add same setup with fast brach

--- react/memo-useMemo-useCallback/useMemo-useCallbak-memo-setup/src/App.jsx
+++ react/memo-useMemo-useCallback/useMemo-useCallbak-memo-setup/src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import ProductsList from "./components/ProductsList";
 import productList from "./data/productList";
 
 function App() {
+  const [showCreator, setShowCreator] = useState(false);
+
+  function handleToggleCreator() {
+    setShowCreator((prev) => !prev);
+  }
+
   return (
     <>
       <h1>Welcome our Shop</h1>
       <ProductsList products={productList} />
+      {showCreator && <p>Berat Yilmaz</p>}
+      <button onClick={handleToggleCreator}>Toggle Creator</button>
     </>
   );
 }
