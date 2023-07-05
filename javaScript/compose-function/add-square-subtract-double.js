7ad3c249e22dc9a731856f70d386ecf114f20518
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-07-05 20:27:44 +0200

    Create compose function

---
+++ javaScript/compose-function/add-square-subtract-double.js
@@ -0,0 +1,18 @@
+// Define some functions for the example
+const addTwo = (x) => x + 2;
+const square = (x) => x ** 2;
+const subtractThree = (x) => x - 3;
+const double = (x) => x * 2;
+
+// Define the compose function
+const compose =
+  (...functions) =>
+  (x) =>
+    functions.reduce((acc, fn) => fn(acc), x);
+
+// Compose the functions into a single function
+const composedFunction = compose(addTwo, square, subtractThree, double);
+
+// Test the result
+const result = composedFunction(5);
+console.log(result); // Output: 51
