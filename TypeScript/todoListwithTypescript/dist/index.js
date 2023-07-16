c926e1cad76e49b4ed387927108209ee485652a5
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-07-16 16:59:43 +0200

    typeScript(basicTodo): Implement add to do button

--- TypeScript/todoListwithTypescript/dist/index.js
+++ TypeScript/todoListwithTypescript/dist/index.js
@@ -2,6 +2,15 @@
 const btn = document.getElementById('btn'); // type comes from tsconfig lib:[] , default changes depends on target and ! is Non-Null Assertion Operator
 const input = document.getElementById('input'); // type assertion
 const form = document.querySelector('form'); // if you write element name directly typeScript can recognize correct type without type assertion
+const toDoList = document.querySelector('#toDoContainer');
 form.addEventListener('submit', function (e) {
     e.preventDefault(); // according to context typeScript can determine type of event
+    const newTodoText = input.value;
+    const newLI = document.createElement('li');
+    newLI.textContent = newTodoText;
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    newLI.append(checkBox);
+    toDoList === null || toDoList === void 0 ? void 0 : toDoList.append(newLI); // without non-null assertion operator (!), you must add ? in order to handle null case 
+    input.value = '';
 });
--- TypeScript/todoListwithTypescript/src/index.ts
+++ TypeScript/todoListwithTypescript/src/index.ts
@@ -1,11 +1,18 @@
 const btn = document.getElementById('btn')! as HTMLButtonElement; // type comes from tsconfig lib:[] , default changes depends on target and ! is Non-Null Assertion Operator
 const input = document.getElementById('input')! as HTMLInputElement; // type assertion
 const form = document.querySelector('form')! // if you write element name directly typeScript can recognize correct type without type assertion
-
-
+const toDoList = document.querySelector('#toDoContainer');
 
 form.addEventListener('submit', function(e){
   e.preventDefault(); // according to context typeScript can determine type of event
+  const newTodoText = input.value;
+  const newLI = document.createElement('li');
+  newLI.textContent = newTodoText; 
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  newLI.append(checkBox);
+  toDoList?.append(newLI); // without non-null assertion operator (!), you must add ? in order to handle null case 
 
+  input.value= ''
 })
 
