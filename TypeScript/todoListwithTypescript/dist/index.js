84fcf9edda599a02a498d6c5a14e8b74c83ca63c
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-07-18 00:37:23 +0200

    Typescript(toDoList): Complete

--- TypeScript/todoListwithTypescript/dist/index.js
+++ TypeScript/todoListwithTypescript/dist/index.js
@@ -1,16 +1,51 @@
 "use strict";
-const btn = document.getElementById('btn'); // type comes from tsconfig lib:[] , default changes depends on target and ! is Non-Null Assertion Operator
-const input = document.getElementById('input'); // type assertion
-const form = document.querySelector('form'); // if you write element name directly typeScript can recognize correct type without type assertion
-const toDoList = document.querySelector('#toDoContainer');
-form.addEventListener('submit', function (e) {
-    e.preventDefault(); // according to context typeScript can determine type of event
-    const newTodoText = input.value;
-    const newLI = document.createElement('li');
-    newLI.textContent = newTodoText;
-    const checkBox = document.createElement('input');
-    checkBox.type = 'checkbox';
+const btn = document.getElementById("btn");
+const input = document.getElementById("input");
+const form = document.querySelector("form");
+const toDoList = document.querySelector("#toDoContainer");
+let toDos = readToDos();
+console.log(toDos);
+toDos.forEach(createToDo);
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    toDos.push(newTodo);
+    createToDo(newTodo);
+    saveToDos();
+    input.value = "";
+});
+function createToDo(todo) {
+    const newLI = document.createElement("li");
+    newLI.textContent = todo.text;
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.checked = todo.completed;
     newLI.append(checkBox);
-    toDoList === null || toDoList === void 0 ? void 0 : toDoList.append(newLI); // without non-null assertion operator (!), you must add ? in order to handle null case 
-    input.value = '';
+    toDoList === null || toDoList === void 0 ? void 0 : toDoList.append(newLI);
+}
+function readToDos() {
+    const toDosJSON = localStorage.getItem('toDos');
+    console.log(toDosJSON);
+    if (toDosJSON === null)
+        return [];
+    return JSON.parse(toDosJSON);
+}
+function saveToDos() {
+    localStorage.setItem('toDos', JSON.stringify(toDos));
+}
+toDoList === null || toDoList === void 0 ? void 0 : toDoList.addEventListener('change', function (e) {
+    var _a, _b;
+    if (e.target instanceof HTMLInputElement && e.target.type === 'checkbox') {
+        const todoText = (_b = (_a = e.target.parentElement) === null || _a === void 0 ? void 0 : _a.textContent) === null || _b === void 0 ? void 0 : _b.trim(); // Add conditional checks
+        if (todoText) {
+            const todo = toDos.find(todo => todo.text === todoText);
+            if (todo) {
+                todo.completed = e.target.checked;
+                saveToDos();
+            }
+        }
+    }
 });
--- TypeScript/todoListwithTypescript/src/index.ts
+++ TypeScript/todoListwithTypescript/src/index.ts
@@ -1,18 +1,64 @@
-const btn = document.getElementById('btn')! as HTMLButtonElement; // type comes from tsconfig lib:[] , default changes depends on target and ! is Non-Null Assertion Operator
-const input = document.getElementById('input')! as HTMLInputElement; // type assertion
-const form = document.querySelector('form')! // if you write element name directly typeScript can recognize correct type without type assertion
-const toDoList = document.querySelector('#toDoContainer');
+const btn = document.getElementById("btn")! as HTMLButtonElement;
+const input = document.getElementById("input")! as HTMLInputElement;
+const form = document.querySelector("form")!;
+const toDoList = document.querySelector("#toDoContainer");
 
-form.addEventListener('submit', function(e){
-  e.preventDefault(); // according to context typeScript can determine type of event
-  const newTodoText = input.value;
-  const newLI = document.createElement('li');
-  newLI.textContent = newTodoText; 
-  const checkBox = document.createElement('input');
-  checkBox.type = 'checkbox';
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
+let toDos: Todo[] = readToDos();
+console.log(toDos);
+toDos.forEach(createToDo);
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  toDos.push(newTodo);
+  createToDo(newTodo);
+  saveToDos();
+  input.value = "";
+});
+
+function createToDo(todo: Todo) {
+  const newLI = document.createElement("li");
+  newLI.textContent = todo.text;
+
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  checkBox.checked = todo.completed;
   newLI.append(checkBox);
-  toDoList?.append(newLI); // without non-null assertion operator (!), you must add ? in order to handle null case 
+  toDoList?.append(newLI);
+}
 
-  input.value= ''
-})
+function readToDos(): Todo[] {
+  const toDosJSON = localStorage.getItem('toDos');
+  console.log(toDosJSON);
 
+  if (toDosJSON === null) return [];
+
+  return JSON.parse(toDosJSON);
+}
+
+function saveToDos() {
+  localStorage.setItem('toDos', JSON.stringify(toDos));
+}
+
+toDoList?.addEventListener('change', function(e) {
+  if (e.target instanceof HTMLInputElement && e.target.type === 'checkbox') {
+    const todoText = e.target.parentElement?.textContent?.trim(); // Add conditional checks
+
+    if (todoText) {
+      const todo = toDos.find(todo => todo.text === todoText);
+
+      if (todo) {
+        todo.completed = e.target.checked;
+        saveToDos();
+      }
+    }
+  }
+});
