3523e073fc2623d667b089ac2beaafb93e5fb12f
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-07-18 00:57:08 +0200

    Typescript(notes): Add classes syntax

---
+++ TypeScript/classes.ts
@@ -0,0 +1,6 @@
+class Person_ {
+  private id:number = 98341234123412341234123412342134213752304850; // you can use vanilla Js #
+  constructor(public first:string= 'berat', public last:string='yilmaz'){ // that is a shortcut, you don't need to write this.first etc.
+
+  }
+}
