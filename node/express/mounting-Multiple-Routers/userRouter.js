e18e84ca2e69e88bfd376da879ca8c6a4a15e08d
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-08-04 18:09:01 +0200

    express: Change user middleware

--- node/express/mounting-Multiple-Routers/userRouter.js
+++ node/express/mounting-Multiple-Routers/userRouter.js
@@ -6,12 +6,18 @@ userRouter.param("id", checkID);
 userRouter.route("/").get(getAllUsers).post(createUser);
 userRouter.route("/:id").get(getUser).patch(updateUser);
 
+const users = [
+  { id: 1, name: "John Doe", age: 25 },
+  { id: 2, name: "Jane Smith", age: 30 },
+  { id: 3, name: "Michael Johnson", age: 28 },
+];
+
 function checkID(req, res, next, val) {
-  const id = users.find((user) => user.id === parseInt(val));
-  if (!id) {
+  const user = users.find((user) => user.id === parseInt(val));
+  if (!user) {
     return res.status(404).send({
       status: "fail",
-      message: "InvalidID",
+      message: "Invalid ID",
     });
   }
   next();
@@ -19,18 +25,27 @@ function checkID(req, res, next, val) {
 
 function getAllUsers(req, res) {
   // Logic to get all users
+  res.status(200).json(users);
 }
 
 function createUser(req, res) {
   // Logic to create a new user
+  const newUser = { id: users.length + 1, ...req.body };
+  users.push(newUser);
+  res.status(201).json(newUser);
 }
 
 function getUser(req, res) {
   // Logic to get a specific user by ID
+  const user = users.find((user) => user.id === parseInt(req.params.id));
+  res.status(200).json(user);
 }
 
 function updateUser(req, res) {
   // Logic to update a user by ID
+  const user = users.find((user) => user.id === parseInt(req.params.id));
+  Object.assign(user, req.body);
+  res.status(200).json(user);
 }
 
 module.exports = userRouter;
