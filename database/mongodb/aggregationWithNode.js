ba2440d7c050a5c5ba61b48a424dce013c3d74bd
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-08-10 09:05:33 +0200

    mongodb: Add $sort and $project stage examples

--- database/mongodb/aggregationWithNode.js
+++ database/mongodb/aggregationWithNode.js
@@ -4,14 +4,37 @@ const collection_name = "accounts";
 const accountsCollection = client.db(dbname).collection(collection_name);
 
 const pipeline = [
+  // $match , $group
   // Stage 1: match the accounts with a balance greater than $1,000
   { $match: { balance: { $lt: 1000 } } },
   // Stage 2: Calculate average balance and total balance
   {
     $group: {
+      // based on key
       _id: "$account_type",
       total_balance: { $sum: "$balance" },
       avg_balance: { $avg: "$balance" },
+    },
+  },
+];
+
+const pipeline2 = [
+  // $sort , $project
+  // Stage 1: $match - filter the documents (checking, balance >= 1500)
+  { $match: { account_type: "checking", balance: { $gte: 1500 } } },
+
+  // Stage 2: $sort - sorts the documents in descending order (balance)
+  { $sort: { balance: -1 } },
+
+  // Stage 3: $project - project only the requested fields and one computed field (account_type, account_id, balance, gbp_balance)
+  {
+    $project: {
+      _id: 0,
+      account_id: 1,
+      account_type: 1,
+      balance: 1,
+      // GBP stands for Great British Pound
+      gbp_balance: { $divide: ["$balance", 1.3] },
     },
   },
 ];
