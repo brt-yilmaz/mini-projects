4332d1d046e21093dce794da827a3fa818dba1b7
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-08-22 22:24:28 +0200

    node-auth: Add auth controller

---
+++ node/Authentication-Authorization-Security/controllers/authController.js
@@ -0,0 +1,15 @@
+const User = require("./models/userModel");
+
+exports.signup = async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        newUser,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
