16d1faacf438ae972a845200a9375db93238a660
Berat Yilmaz <berat@testbee.ai>
2024-04-07 19:12:45 +0200

    typescript[advanced]: add better approach to remove multiple union

--- TypeScript/advanced/removeAndReplaceUnion.ts
+++ TypeScript/advanced/removeAndReplaceUnion.ts
@@ -1,8 +1,25 @@
 type Country = 'us' | 'uk' | 'france' | 'germany' | 'russia';
 
-type RemoveRussia<T> = T extends 'russia' ? 'The world is better place' : T;
+type RemoveRussia<T> = T extends 'russia' ? 'The world is better place' : T
 
 type CountryWithoutRussia = RemoveRussia<Country>
 
 // you can not refine the type
 type BetterWorld = Exclude<Country, 'russia'>
+
+// you can use multiple extends
+type RemoveRussiaOrGermany<T> = 
+  T extends 'russia' ? 'The world is better place' :
+  T extends 'germany' ? 'A beer heaven' :
+  T;
+
+type CountryWithoutRussiaOrGermany = RemoveRussiaOrGermany<Country>;
+
+// a better approach
+interface CountryReplacements {
+  russia: 'The world is better place';
+  germany: 'A beer heaven';
+}
+
+type BetterWorld2<T extends Country> = T extends keyof CountryReplacements ? CountryReplacements[T] : T
+type CountryWithoutRussiaOrGermany2 = BetterWorld2<Country>;
